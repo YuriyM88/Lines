791daaf1f368346b39659c0a9300e2056bd1a693
YuriyM <Mikchailov1988@mail.ru>
2025-08-14 12:45:35 +0300

    Tasks

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Задача 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+
+        String fullName = String.join(" ", lastName, firstName, middleName);
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println("Задача 2");
+
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        System.out.println("Задача 3");
+
+        String fullName2 = "Иванов Семён Семёнович";
+        String fullNameNew = fullName2.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameNew);
 
     }
 }
